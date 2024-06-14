c5952e40022d58b5de0121f8ce1a2e93b398f3c7
Rodrigo Henriquez <rodrigohenriquez94@gmail.com>
2024-06-14 15:09:10 -0400

    runBuild

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 // Home.js
 import React from 'react';
-import Earth from '../Components/DetailsPlanets'
+import { Link } from 'react-router-dom';
+import data from '../data.json';
 
 function Home() {
   return (
     <div>
-      <h1 className='text-white text-[90px]'>SOLAR PLANETS</h1>
-     
+      <h1 className="text-white text-[120px] p-48">SOLAR SISTEM</h1>
+      
     </div>
   );
 }
